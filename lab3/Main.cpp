3928794ae295b4ffb76d6d1aee8f19a1d343ebd2
Maxim Kliashchou <61432134+maximkliashchou@users.noreply.github.com>
2020-04-02 16:05:20 +0300

    update lab3

--- lab3/Main.cpp
+++ lab3/Main.cpp
@@ -6,32 +6,76 @@
 
 using namespace std;
 
-int n;
-int mn = INT_MAX - 1, mx = INT_MIN + 1;
-double average = 0;
+typedef struct {
+	double* a;
+	int n, mx, mn;
+	double average;
+}params;
 
-DWORD WINAPI min_maxFunc(LPVOID a) {
-	int* b = static_cast<int*>(a);
-	int m = b[0];
-	for (int j = 1; j <= m; j++) {
-		mn = min(mn, b[j]);
-		Sleep(7);
-		mx = max(mx, b[j]);
+void printMinAndMax(params* params) {
+	cout << "Min; Max:\n" << params->mn << "; " << params->mx << endl;
+}
+
+void printAverage(params* params) {
+	cout << fixed << setprecision(5) << "Average:\n" << params->average << endl;
+}
+
+
+DWORD WINAPI min_maxFunc(LPVOID myParams) {
+	params* v = (params*)myParams;
+	for (int j = 0; j < v->n; j++) {
+		v->mn = min(v->mn, v->a[j]);
+		v->mx = max(v->mx, v->a[j]);
 		Sleep(7);
 	}
+	printMinAndMax(v);
 	return 0;
 }
 
-DWORD WINAPI averageFunc(LPVOID a) {
-	int* b = static_cast<int*>(a);
-	int m = b[0];
+DWORD WINAPI averageFunc(LPVOID myParams) {
+	params* v = (params*)myParams;
 	int sum = 0;
-	for (int j = 1; j <= m; j++) {
-		sum += b[j];
+	for (int j = 0; j < v->n; j++) {
+		sum += v->a[j];
 		Sleep(12);
 	}
-	average = sum / m;
+	v->average = 1.0 * sum / v->n;
+	printAverage(v);
 	return 0;
+}
+
+void readArray(params &params) {
+	cout << "Enter number of elements:\n";
+	cin >> params.n;
+	cout << "Enter elements:\n";
+
+	params.a = new double[params.n];
+
+	for (int i = 0; i < params.n; i++) {
+		cin >> params.a[i];
+	}
+}
+
+void replaceMinAndMaxWithAverage(params params) {
+	for (int i = 0; i < params.n; i++) {
+		if (params.a[i] == params.mn || params.a[i] == params.mx) {
+			params.a[i] = params.average;
+		}
+	}
+}
+
+void printArray(params params) {
+	cout << "Array:\n";
+	for (int i = 0; i < params.n; i++) {
+		cout << params.a[i] << ' ';
+	}
+	cout << endl;
+}
+
+void init(params &myParams) {
+	myParams.mn = INT_MAX - 1;
+	myParams.mx = INT_MIN + 1;
+	myParams.average = 0;
 }
 
 int main() {
@@ -40,26 +84,22 @@ int main() {
 
 	HANDLE hThreadAverage;
 	DWORD IDThreadAverage;
-	cout << "Enter number of elements:\n";
-	cin >> n;
-	cout << "Enter elements:\n";
-	vector<int> v(n);
-	int* a = new int[n + 1];
-	a[0] = n;
-	for (int i = 1; i <= n; i++) cin >> a[i];		
+	params myParams;
+	init(myParams);
 
-	hThreadMinMax = CreateThread(NULL, 0, min_maxFunc, (void*)a, 0, &IDThreadMinMax);
+	readArray(myParams);
+				
+	hThreadMinMax = CreateThread(NULL, 0, min_maxFunc, (void*)&myParams, 0, &IDThreadMinMax);
+
+	hThreadAverage = CreateThread(NULL, 0, averageFunc, (void*)&myParams, 0, &IDThreadAverage);
+
 	WaitForSingleObject(hThreadMinMax, INFINITE);
-	TerminateThread(hThreadMinMax, 0);
-	CloseHandle(hThreadMinMax);
-	cout << "Min; Max: " << mn << "; " << mx << endl;
-
-	hThreadAverage = CreateThread(NULL, 0, averageFunc, (void*)a, 0, &IDThreadAverage);
 	WaitForSingleObject(hThreadAverage, INFINITE);
 	TerminateThread(hThreadAverage, 0);
+	TerminateThread(hThreadMinMax, 0);
 	CloseHandle(hThreadAverage);
-	cout << fixed << setprecision(5) << "Average: " << average << endl;
-	
+	CloseHandle(hThreadMinMax);
 
-
+	replaceMinAndMaxWithAverage(myParams);
+	printArray(myParams);
 }
